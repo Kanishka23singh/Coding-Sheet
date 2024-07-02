8c74efd223984be603edd080e08ee5762317a65e
Kanishka23singh <92868964+Kanishka23singh@users.noreply.github.com>
2024-07-02 23:27:58 +0530

    Update and rename All factors of a number.java to 05.All factors of a number.java

--- Number/05.All factors of a number.java
+++ Number/05.All factors of a number.java
@@ -1,4 +1,23 @@
+/*
+ Input : n = 10       
+ Output: 1 2 5 10
 
+ Input:  n = 100
+ Output: 1 2 4 5 10 20 25 50 100
+
+ Input:  n = 125
+ Output: 1 5 25 125 
+
+
+Remember occurs in pairs.
+For example:
+Factors of 8 : 1 * 8, 2 * 4 --> [1,8,2,4]
+Factors of 9 : 1 * 9, 3 * 4 --> [1,9,3]
+Factors of 12 : 1 * 12, 2 * 6, 3 * 4 --> [1,12,2,6,3,4]
+
+So, if 'i' is factor of 'n' say
+Then factor pair of 'i' will be 'n/i'
+*/
 class Factors {
   public static List<Integer> allFactors(int n) {
       List<Integer> factors = new ArrayList<>(); // Initialize the factors list
