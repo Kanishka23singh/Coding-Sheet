24f904a6e850645cf8a60bf65b69661200fa08cb
Kanishka23singh <92868964+Kanishka23singh@users.noreply.github.com>
2024-07-09 00:03:07 +0530

    Update 01. Check for Palindrome String.java

--- String/01. Check for Palindrome String.java
+++ String/01. Check for Palindrome String.java
@@ -3,7 +3,7 @@ public class Solution {
 	public static boolean checkPalindrome(String str) {
 	
 		// It replaces any character that isn't in the set a-z, A-Z, or 0-9 with nothing.
-		str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+		str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase(); // most important line
 
 		int left = 0;
 		int right = str.length() - 1;
