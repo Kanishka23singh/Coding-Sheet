95eedfc6aa970dde38640283941ed81f8c45bb8f
Kanishka23singh <92868964+Kanishka23singh@users.noreply.github.com>
2024-07-14 23:59:50 +0530

    Create 	Count Odd Numbers in an Interval Range.java

---
+++ Accenture/Count Odd Numbers in an Interval Range.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countOdds(int low, int high) {
+        return (high + 1) / 2 - low / 2;
+    }
+}
+
+/*
+class Solution {
+    public int countOdds(int low, int high) {
+        int cnt = 0;
+        for(int i = low; i <= high; i++){
+            if(i % 2 != 0){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
+*/
